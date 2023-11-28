4de934a634d14a225de1ff3c8b64e4a1c66e65c9
us-mspinola <us.mspinola@formacao.ips.pt>
2023-11-28 00:49:32 +0000

    GetAllApiGenes.searchGeneById(xappToken, apiUrl);

--- src/main/java/g7/upskill/ips/clients/GetAllApi.java
+++ src/main/java/g7/upskill/ips/clients/GetAllApi.java
@@ -5,10 +5,21 @@ import g7.upskill.ips.LigacaoArtsy;
 public class GetAllApi {
 
     public static void main(String[] args) {
-        boolean testMode= false;
+
+
+        boolean test= true;
 
         String xappToken= LigacaoArtsy.generateXappToken();
+        String apiUrl ="";
+        // apiUrl=  "https://api.artsy.net/api/genes/4d90d190dcdd5f44a5000032";
+        apiUrl=  "https://api.artsy.net/api/genes/4de57c607804370001008c8b";
 
-        GetAllApiGenes.searchAllGenes(xappToken, testMode);
+
+        if (test) {
+            GetAllApiGenes.searchGeneById(xappToken, apiUrl);
+        }
+        else {
+            GetAllApiGenes.searchAllGenes(xappToken);
+        }
     }
 }
